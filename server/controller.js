ef2071878ccabb5f2aa1a3ec0895e5f3248a3101
Julien Bongars <julienbongars23@gmail.com>
2018-10-29 03:25:45 +0800

    refactored code

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,7 @@
 
 // const https = require('https');
 const request = require('request');
+request.debug = true;
 const jsdom = require('jsdom').JSDOM;
 
 
@@ -49,25 +50,22 @@ ctrl.genClientHTML = (data) => `
     </html>
 `
 
+ctrl.processJSON = (html) => {
+    const dom = new jsdom(html);
+    const document = dom.window.document;
+
+    return Array.from(document.getElementsByClassName('id3')).map(elem => {
+        return {
+            image: elem.children.item(0).children.item(0).src,
+            href: elem.children.item(0).href,
+        }
+    });
+}
+
 ctrl.scrapeFrontpage = (cb) => {
     makeHTMLReq(process.env.ROOT).then(html => {
-        const dom = new jsdom(html);
-        const document = dom.window.document;
 
-        let images = Array.from(document.getElementsByClassName('id3')).map(elem => {
-            return {
-                image: elem.children.item(0).children.item(0).src,
-                href: elem.children.item(0).href,
-            }
-        });
-
-        // let images = Array.from(document.getElementsByClassName('itg').children).map(elem => {
-        //     return {
-        //         image: elem.children.item(1).children.item(0).children.item(0).src,
-        //         href: elem.children.item(1).children.item(0).href,
-        //     }
-        // })
-
+        const images = ctrl.processJSON(html);
         const responseHTML = ctrl.genClientHTML(images);
         // console.log('responseHTML is: ', responseHTML);
 
