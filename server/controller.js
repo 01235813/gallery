a7aa63fdf5b5970b541380bcf2bcc72e576aba41
Julien Bongars <julienbongars23@gmail.com>
2018-10-30 02:11:35 +0800

    refactored code, added new pages

--- server/controller.js
+++ server/controller.js
@@ -2,13 +2,12 @@
 const request = require('request');
 // request.debug = true;
 const jsdom = require('jsdom').JSDOM;
-const queries = require('./config.json').queries;
-
+const queries = require('./config').queries;
 
 
 ctrl = {};
 
-let makeHTMLReq = (url, page) => new Promise((resolve, reject) => {
+let makeHTMLReq = (url, page=0) => new Promise((resolve, reject) => {
     if(typeof url !== 'string'){
         throw new Error('env.URL is not a string');
     }
@@ -38,48 +37,71 @@ let genClientHTML = (data) => `
     </html>
 `
 
-let processJSON = (html) => {
-    const dom = new jsdom(html);
-    const document = dom.window.document;
+let getJSON = {
+    frontpage: (html) => {
+        const dom = new jsdom(html);
+        const document = dom.window.document;
 
-    let result = [];
+        let result = [];
 
-    Array.from(document.getElementsByClassName('itd')).forEach((elem, i) => {
-        if(
-            typeof elem.getElementsByClassName('it2')[0] !== 'undefined' && 
-            typeof elem.getElementsByClassName('it5')[0] !== 'undefined'
-        ){
-            let image_div = elem.getElementsByClassName('it2')[0];
-            let key = image_div.innerHTML.split('~', 4);
-            let proto = key[0] == 'init' ? 'http' : 'https';
-    
-            result.push({
-                image: `${proto}://${key[1]}/${key[2]}`,
-                href: elem.getElementsByClassName('it5')[0].children[0].href
-            })
-        }
-    })
-    return result;
+        Array.from(document.getElementsByClassName('itd')).forEach((elem, i) => {
+            if(
+                typeof elem.getElementsByClassName('it2')[0] !== 'undefined' && 
+                typeof elem.getElementsByClassName('it5')[0] !== 'undefined'
+            ){
+                let image_div = elem.getElementsByClassName('it2')[0];
+                let key = image_div.innerHTML.split('~', 4);
+                let proto = key[0] == 'init' ? 'http' : 'https';
+        
+                result.push({
+                    image: `${proto}://${key[1]}/${key[2]}`,
+                    href: elem.getElementsByClassName('it5')[0].children[0].href
+                })
+            }
+        });
+        return result;
+    }
 }
 
-let preScrapper = (...args) => new Promise(resolve => {
+let scrapper = (option, ...args) => new Promise(resolve => {
+    let processJSON = getJSON[option];
     makeHTMLReq(...args).then(html => {
         let result = processJSON(html);
         resolve(result);
     })
 })
 
-ctrl.scrapeFrontpage = (cb) => {
-    promises = [preScrapper(process.env.ROOT)]
-    for(let i = 1; i < 101; i++){
-        promises.push(preScrapper(process.env.ROOT, i))
+ctrl.preScrapper = (req, res) => new Promise(resolve => {
+    let option = 'frontpage';
+
+    promises = [scrapper(option, process.env.ROOT + req.url)]
+    for(let i = 1; i < process.env.PAGES; i++){
+        promises.push(scrapper(option, process.env.ROOT + req.url, i))
     }
     Promise.all(promises).then(data => {
 
         const responseHTML = genClientHTML([].concat(...data));
-        cb(responseHTML);
+        resolve(responseHTML);
+    });
+});
+
+ctrl.frontPage = (req, res) => {
+    console.log('url = ', req.url);
+    // res.status(200).send('ok');
+
+    ctrl.preScrapper(req, res).then(data => {
+        res.status(200).send(data)
     })
 }
+
+ctrl.articlePage = (req, res) => {
+    res.status(200).send('ok')
+}
+
+ctrl.galleryPage = (req, res) => {
+
+}
+
 
 
 
--- server/index.js
+++ server/index.js
@@ -9,12 +9,9 @@ require('dotenv').config(); // load dotenv constants
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-    // res.status(200).send('hello world');
-    controller.scrapeFrontpage(data => {
-        res.status(200).send(data)
-    })
-})
+app.get('/g*', controller.articlePage);
+app.get('/s*', controller.galleryPage);
+app.get('/', controller.frontPage);
 
 app.listen(PORT);
 console.log('server running on port:', PORT);
