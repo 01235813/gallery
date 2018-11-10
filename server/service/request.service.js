f63141a9a89176368549bed673e3074a1bb509ff
Julien Bongars <julienbongars23@gmail.com>
2018-11-10 11:05:08 +0800

    refactor code

--- server/service/request.service.js
+++ server/service/request.service.js
@@ -4,7 +4,9 @@ const request = require('request');
 const jsdom = require('jsdom').JSDOM;
 // request.debug = true;
 
-let get = (url, queries={}) => new Promise((resolve, reject) => {
+svc = {};
+
+svc.get = (url, queries={}) => new Promise((resolve, reject) => {
     if(typeof url !== 'string'){
         throw new Error('env.URL is not a string');
     }
@@ -19,12 +21,9 @@ let get = (url, queries={}) => new Promise((resolve, reject) => {
     })
 })
 
-let getDOC = (url, queries={}) => new Promise((resolve, reject) => {
+svc.getDOC = (url, queries={}) => new Promise((resolve, reject) => {
     let wrapDOM = (req) => new jsdom(req).window.document
     get(url, queries).then(body => wrapDOM(body)).then(resolve);
 })
 
-module.exports = {
-    get: get,
-    getDOC: getDOC
-}
+module.exports = svc;
