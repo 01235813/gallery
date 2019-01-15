29e0bb763e03a9aaa3ee5abeed7c93fe64a33eb1
Julien Bongars <pri.julien@gmail.com>
2019-01-16 00:27:27 +0800

    updated example config

--- server/config.example.js
+++ server/config.example.js
@@ -1,4 +1,12 @@
-
 module.exports = {
-    "queries": {    }
+    "headers": {
+        "cookie": "nw=1; tagaccept=1; sl=dm_1"
+    },
+    "frontPage": {
+        "queries": {
+            "f_apply": "Apply+Filter"
+
+        }
+    },
+    "articlePage": {}
 }
